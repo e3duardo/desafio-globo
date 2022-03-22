9bec29d5ef1b2ffa3ca74ab238ec2c705245eaa1
Eduardo Santos <e3duardo@gmail.com>
2022-03-21 23:04:16 -0300

    wrk benchmark config

---
+++ infra/benchmark.lua
@@ -0,0 +1,4 @@
+wrk.method = "POST"
+wrk.body   = "{ \"brother_id\": 1 }"
+wrk.headers["Authorization"] = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozLCJleHAiOjE2NDc5OTUyMzR9.FgxQP9kjfnVoT3HJIs8mxWtb7UG6-0yz0qiDMlc7mp0'
+wrk.headers["Content-Type"] = "application/json"
